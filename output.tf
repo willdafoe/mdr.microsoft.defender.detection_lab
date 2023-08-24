6132d4b0da06eef02155b0c96b84ece94dd9df2d
ralph-brynard <ralph@thebrynards.com>
2023-08-24 15:07:58 -0500

    workflow file

--- output.tf
+++ output.tf
@@ -9,15 +9,3 @@ output "virtual_network_name" {
 output "domain_controller_public_ip" {
   value = module.windows_virtual_machine["domain_controller"].public_ip_address
 }
-
-#output "domain_controller_ip" {
-#  value = !(lookup(local.config.WINDOWS_VIRTUAL_MACHINE, #"domain_controller", null) == null) ? { for k, instance in module.#windows_virtual_machine["domain_controller"] : k => instance.#public_ip_address } : null
-#}
-
-#output "windows_server_ip" {
-#  value = !(lookup(local.config.WINDOWS_VIRTUAL_MACHINE, "windows_server", null) == null) ? { for k, instance in module.windows_virtual_machine["windows_server"] : k => instance.public_ip_address } : null
-#}
-
-#output "windows_client_ip" {
-#  value = !(lookup(local.config.WINDOWS_VIRTUAL_MACHINE, "windows_client", null) == null) ? { for k, instance in module.windows_virtual_machine["windows_client"] : k => instance.public_ip_address } : null
-#}
